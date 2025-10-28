8f685cf43dd2c9a79d36027530c359351d67edf9
uselessgoddess <dodickgod@gmail.com>
2025-10-28 16:40:55 +0100

    add header definition

---
+++ include/arc.hxx
@@ -0,0 +1,19 @@
+#include <cstdint>
+#include <array>
+
+constexpr std::array<uint8_t, 4> BAR = {0xf0, 0x9f, 0x8d, 0xbe};
+
+enum struct entry_type : uint8_t { reg = 0, dir = 1, sym = 2 };
+
+#pragma pack(push, 1)
+struct header_t {
+  entry_type type;
+  uint8_t _____;  // reserved for 40 bytes
+  int64_t mtime;
+  uint16_t path;
+  uint32_t mode;
+  uint64_t data;
+};  // 1 + 1 + 8 + 2 + 4 + 8
+#pragma pack(pop)
+
+static_assert(sizeof(header_t) == 24);
